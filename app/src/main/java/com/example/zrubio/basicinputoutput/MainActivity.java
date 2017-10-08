30a34d873954309f0c482d13c0cbf66e8f684bfd
Zaira Rubio <zrubio@protonmail.com>
2017-10-08 23:25:31 +0200

    MainActivity.java changed. Created EditText and Button objects. User introduce text in EditText and when he clicks on Button, the app shows a Toast message with the text introduced at first.

--- app/src/main/java/com/example/zrubio/basicinputoutput/MainActivity.java
+++ app/src/main/java/com/example/zrubio/basicinputoutput/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.zrubio.basicinputoutput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText editText;
+    public Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.inputName);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
     }
 }
